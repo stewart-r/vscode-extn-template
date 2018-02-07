74144aefbb39bd850df25512a21a4655cd63175e
Stewart Robertson <Stewart.Robertson@searchlaboratory.com>
2018-02-07 12:49:25 +0000

    wired up command

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,15 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    console.log(`extension activated!`);
+    console.log(`text-tools extension activated!`);
+    let insertAboveCommand = vscode.commands.registerTextEditorCommand("textTools.insertLine",() => {
+        console.log('insertLine command called');
+    })
     
+    context.subscriptions.push(insertAboveCommand);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-    console.log(`image-comments extension activated!`);
+    console.log(`text-tools extension deactivated`);
 }
